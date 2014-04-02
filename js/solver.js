076a20b8a1cc6f080def4ae9ae478c317a358c90
Masashi Nakata <agw@t3.rim.or.jp>
2014-04-02 12:33:15 -0700

    Removed shuffle internal function.

--- js/solver.js
+++ js/solver.js
@@ -101,7 +101,7 @@ Solver.prototype.solve = (function () {
 	  var size  = cells.length;
 
 	  if (size) {
-	    shuffle(cells);
+	    cells.shuffle();
 
 	    cells.splice(Math.floor(Math.random() * MAX_CELLS + 1));
 
@@ -130,11 +130,6 @@ Solver.prototype.solve = (function () {
     
     return scores;
   };
-
-  function shuffle(a) {
-    for (var i = a.length, j; j = Math.floor(Math.random() * i), i --; )
-      a[i] = [a[j], a[j] = a[i]][0];
-  }
 
   function max_direction(scores) {
     var max_direction = -1, max_score = -1;
